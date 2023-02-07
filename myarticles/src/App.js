c3ac67ca85204b95d458b233e81ad8dca117ad71
Dan Carlier <dan.carlier@gmail.com>
2023-02-07 15:30:43 +1100

    Add react project

---
+++ myarticles/src/App.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react'
+import './App.css';
+
+function App() {
+  const [articles, setArticles] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => setArticles(data))
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>all articles</h1>
+        {articles && articles.map(article => (
+          <div key={article.id}>{article.title}</div>
+        ))}
+      </header>
+    </div>
+  );
+}
+
+export default App;
